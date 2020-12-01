37a182632ccfe912126e3d8c0703ca6b69a524e6
Andrew Quackenbos <a.quackenbos@gmail.com>
2020-12-01 03:48:10 -0500

    Minor updates

--- src/App.vue
+++ src/App.vue
@@ -35,11 +35,24 @@
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+  transition: opacity .3s;
 }
-
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+.slide-enter-active {
+  transition: all .1s ease;
+}
+.slide-leave-active {
+  transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(1000px);
 }
 </style>
 
